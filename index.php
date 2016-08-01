fab8fc4dcf0d84f8c9d7504455b896e0c9dec560
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-01 12:59:56 -0600

    map is functioning with js and css files

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 	<html lang="en">
+	<meta charset="UTF-8">
 	<head>
-		<meta charset="UTF-8">
-		<title>GOOGLE MAPS API SANDBOX</title>
+		<script src="js.js" type="text/javascript"></script>
+		<link href="style.css" rel="stylesheet" type="text/css" />
 	</head>
+
+
 	<body>
-		I AM SO EMPTY
+		<h3>My Google Maps Demo</h3>
+		<div id="map"></div>
 
 		<script async defer
-				  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-875XeE90oXcty5RUEalsmAvwbFGvZTI&v=3&callback=initMap">
-
+				  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-875XeE90oXcty5RUEalsmAvwbFGvZTI&callback=initMap">
 		</script>
 	</body>
 </html>
---
+++ js.js
@@ -0,0 +1,9 @@
+
+function initMap() {
+	var mapDiv = document.getElementById('map');
+	var map = new google.maps.Map(mapDiv, {
+		center: {lat: 35.113281, lng: -106.621216},
+		zoom: 8
+	});
+}
+
